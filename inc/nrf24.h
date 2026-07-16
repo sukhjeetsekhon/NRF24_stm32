91483f13576c0dff414db00ddfe45134db1fc20c
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-16 09:41:55 -0700

    fix: set up nrf24.h/c

---
+++ inc/nrf24.h
@@ -0,0 +1,18 @@
+/**
+   @file nrf24.h
+
+   @brief
+
+   @see
+
+*/
+
+#ifndef NRF24_H
+#define NRF24_H
+
+
+
+
+
+
+#endif /* NRF24_H */
---
+++ src/nrf24.c
@@ -0,0 +1,11 @@
+/**
+   @file nrf24.c
+
+   @brief
+
+   @see
+
+*/
+
+#include "nrf24.h"
+

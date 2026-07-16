92ee099fc9aee600efcd973544bb1444806add57
sukhjeetsekhon <sjtsekhon@gmail.com>
2026-07-16 07:47:01 -0700

    fix: change function names from pascal case to snake case

--- inc/nrf24.h
+++ inc/nrf24.h
@@ -59,7 +59,7 @@ enum {
  * NOTICE: Don't use it at start of program because it may broke down process
  * 		   after reseting the MCU. This function is for only special situations or testing.
  */
-void nrf24SetDefaults(void);
+void nrf24_set_defaults(void);
 
 
 /*
@@ -68,39 +68,39 @@ void nrf24SetDefaults(void);
  * NOTICE: Without this function in start of program may cause problems after reseting MCU.
  * 		   MCU reset does not resets NRF24 module
  */
-void nrf24Init(void);
+void nrf24_init(void);
 
 
 //These functions are for controll CE and CSN pins which are selected in nrf24_config.h
-void writeCSNHIGH(void);
-void writeCSNLOW(void);
-void writeCEHIGH(void);
-void writeCELOW(void);
+void write_csn_high(void);
+void write_csn_low(void);
+void write_ce_high(void);
+void write_ce_low(void);
 
 
 //For read or write nrf24 registers via spi bus
-void nrf24WriteReg(uint8_t reg, uint8_t *data, uint8_t size);
-uint8_t nrf24ReadReg(uint8_t reg, uint8_t size);
+void nrf24_write_reg(uint8_t reg, uint8_t *data, uint8_t size);
+uint8_t nrf24_read_reg(uint8_t reg, uint8_t size);
 
 
 //For write some special commands such as  W_TX_PAYLOAD, FLUSH_RX, ETC
-void nrf24WriteSpecialCommand(uint8_t cmd);
+void nrf24_write_special_command(uint8_t cmd);
 
 
 /*  One of these functions are used after "nrf24_w_spec_cmd" for execute
  * command which means reading or writing relevant Buffer. this functions
  * are not necessary after all special commands such as FLUSH_TX or FLUSH_RX
  */
-void nrf24WriteSpecialReg(uint8_t *data, uint8_t size);
-void nrf24ReadSpecialReg(uint8_t *data, uint8_t size);
+void nrf24_write_special_reg(uint8_t *data, uint8_t size);
+void nrf24_read_special_reg(uint8_t *data, uint8_t size);
 
 
 //Power up nrf24 module
-void nrf24PowerUp(void);
+void nrf24_power_up(void);
 
 
 //Power down nrf24 module
-void nrf24PowerDown(void);
+void nrf24_power_down(void);
 
 
 /* Control nrf24 TX power:
@@ -114,7 +114,7 @@ void nrf24PowerDown(void);
  * _0dbm  = 3 -> Maximum power
  *
  */
-void nrf24SetTXPower(uint8_t pwr);
+void nrf24_set_tx_power(uint8_t pwr);
 
 
 /* Set NRF24 data rate:
@@ -126,7 +126,7 @@ void nrf24SetTXPower(uint8_t pwr);
  * _250kbps = 2
  *
  */
-void nrf24SetDataRate(uint8_t bps);
+void nrf24_set_data_rate(uint8_t bps);
 
 
 /*Set channel from 0 to 125 (In summary it has 126 channel from 1400Mhz to 1525Mhz)
@@ -138,19 +138,19 @@ void nrf24SetDataRate(uint8_t bps);
  *  ....
  * 1525Mhz -> 125
  */
-void nrf24SetChannel(uint8_t ch);
+void nrf24_set_channel(uint8_t ch);
 
 
 /*Write here pipe address, for example:
  * uint8_t addr[5] = { 0x53, 0x13, 0x01, 0x75, 0x82 };
  */
-void nrf24OpenTXPipe(uint8_t *addr);
+void nrf24_open_tx_pipe(uint8_t *addr);
 
 
 /*
  *  Control payload size for each pipe individually (from 0 to 32)
  */
-void nrf24SetPipePayloadSize(uint8_t pipe, uint8_t size);
+void nrf24_set_pipe_payload_size(uint8_t pipe, uint8_t size);
 
 
 /*
@@ -159,13 +159,13 @@ void nrf24SetPipePayloadSize(uint8_t pipe, uint8_t size);
  * NOTICE: if you are using acknowledgement feature it is obligatory
  *         to set same address to TX and RX 0 pipe.
  */
-void nrf24OpenRXPipe(uint8_t pipe, uint8_t *addr);
+void nrf24_open_rx_pipe(uint8_t pipe, uint8_t *addr);
 
 
 /*
  * Close opened RX pipe
  */
-void nrf24CloseRXPipe(uint8_t pipe);
+void nrf24_close_rx_pipe(uint8_t pipe);
 
 
 /*
@@ -179,7 +179,7 @@ void nrf24CloseRXPipe(uint8_t pipe);
  * 1byte = 0
  *_2byte = 1
  */
-void nrf24SetCRC(uint8_t en_crc, uint8_t crc0);
+void nrf24_set_crc(uint8_t en_crc, uint8_t crc0);
 
 
 /*
@@ -188,21 +188,21 @@ void nrf24SetCRC(uint8_t en_crc, uint8_t crc0);
  * won't be able to use this: uint8_t addr[5] = { 0x53, 0x13, 0x01, 0x75, 0x82 };
  * you need to shorten it for instance: uint8_t addr[3] = { 0x53, 0x13, 0x01 };
  */
-void nrf24SetAddressWidth(uint8_t bytes);
+void nrf24_set_address_width(uint8_t bytes);
 
 
 //This function is for clear TX FIFO fully
-void nrf24FlushTX(void);
+void nrf24_flush_tx(void);
 
 
 //This function is for clear RX FIFO fully
-void nrf24FlushRX(void);
+void nrf24_flush_rx(void);
 
 
 /*
  * Read Status register correctly without interfering in processes
  */
-uint8_t nrf24ReadStatusReg(void);
+uint8_t nrf24_read_status_reg(void);
 
 
 /*
@@ -210,7 +210,7 @@ uint8_t nrf24ReadStatusReg(void);
  * after receiving data in RX FIFO and it must be cleared by writing 1 which
  * sets it to 0
  */
-void nrf24ClearRX_DR(void);
+void nrf24_clear_rx_dr(void);
 
 
 /*
@@ -218,7 +218,7 @@ void nrf24ClearRX_DR(void);
  * after Transmitting data or receiving acknowledgement and it must be cleared
  * by writing 1 which sets it to 0
  */
-void nrf24ClearTX_DS(void);
+void nrf24_clear_tx_ds(void);
 
 
 /*
@@ -226,101 +226,113 @@ void nrf24ClearTX_DS(void);
  * after auto retransmissions will exceed limit and it must be cleared by writing 1 which
  * sets it to 0
  */
-void nrf24ClearMAX_RT(void);
+void nrf24_clear_max_rt(void);
 
 
 /*
  * Read individual bit in preferred register
  */
-uint8_t nrf24ReadBit(uint8_t reg, uint8_t bit);
+uint8_t nrf24_read_bit(uint8_t reg, uint8_t bit);
+
 
 
 /*
  * Set individual bit in preferred register
  */
-void nrf24SetBit(uint8_t reg, uint8_t bit, uint8_t val);
+void nrf24_set_bit(uint8_t reg, uint8_t bit, uint8_t val);
+
 
 
 /*
  * Read received payload size which is also useful for prevent data which size
  * is more than 32 bytes by flushing it "nrf24_flush_rx()"
  */
-uint8_t nrf24ReadPayloadWidth(void);
+uint8_t nrf24_read_payload_width(void);
+
 
 
 /*
  * This function turns nrf24 to RX mode also it sets CE pin HIGH
  */
-void nrf24SwitchToRX(void);
+void nrf24_switch_to_rx(void);
 
 
 /*
  * This function is for turn off RX mode and go in TX mode
  */
-void nrf24SwitchToTX(void);
+void nrf24_switch_to_tx(void);
 
 
 /*
  * Enable or disable Dynamic PayLoad by writing 'enable' or 'disable' as an argument
  */
-void nrf24EnableDynamicPayload(uint8_t en);
+void nrf24_enable_dynamic_payload(uint8_t en);
 
 
 /*
  * Set individual pipe for dynamic payload.
  * This function must be used after "nrf24EnableDynamicPayload(uint8_t en)" function
  */
-void nrf24SetRXDynamicPayloadPipe(uint8_t pipe, uint8_t en);
+void nrf24_set_rx_dynamic_payload_pipe(uint8_t pipe, uint8_t en);
+
 
 
 /*
  * Enable or disable auto acknowledgement for each pipe
  */
-void nrf24EnableAutoAck(uint8_t pipe, uint8_t ack);
+void nrf24_enable_auto_ack(uint8_t pipe, uint8_t ack);
+
 
 
 /*
  * Enable or disable auto acknowledgement for all pipe together
  */
-void nrf24EnableAutoAckOnAllPipes(uint8_t ack);
+void nrf24_enable_auto_ack_on_all_pipes(uint8_t ack);
+
 
 
 /*
  * Enable or disable acknowledgement with user defined payload
  */
-void nrf24EnableAckPayload(uint8_t en);
+void nrf24_enable_ack_payload(uint8_t en);
+
 
 
 /*
  * Enable or disable "nrf24_transmit_no_ack"
  */
-void nrf24EnableTransmitNoAck(uint8_t en);
+void nrf24_enable_transmit_no_ack(uint8_t en);
+
 
 
 /*
  * Set delay between auto-retransmissions
  */
-void nrf24SetAutoRetransmissionDelay(uint8_t delay);
+void nrf24_set_auto_retransmission_delay(uint8_t delay);
+
 
 
 /*
  * Set auto-retransmissions limit
  */
-void nrf24SetAutoRetransmissionLimit(uint8_t limit);
+void nrf24_set_auto_retransmission_limit(uint8_t limit);
+
 
 
 /*
  * This function is used to convert the not uint8_t varible into uint8_t array which is
  * necessary for transmit correctly and on timing
  */
-void nrf24ConvertTypeToUint8Array(size_t in, uint8_t* out, uint16_t size);
+void nrf24_convert_type_to_uint8_array(size_t in, uint8_t* out, uint16_t size);
+
 
 
 /*
  * This function converts received uint8_t array to other one data type for example
  * uint16_t, uint32_t
  */
-size_t nrf24ConvertUint8ArrayToType(uint8_t* in, uint16_t size);
+size_t nrf24_convert_uint8_array_to_type(uint8_t* in, uint16_t size);
+
 
 
 /*
@@ -329,51 +341,56 @@ size_t nrf24ConvertUint8ArrayToType(uint8_t* in, uint16_t size);
  * which means target RX device does not responding, it returns 1, this is useful
  * with acknowledgements
  */
-uint8_t nrf24TransmitData(uint8_t *data, uint8_t size);
+uint8_t nrf24_transmit_data(uint8_t *data, uint8_t size);
+
 
 
 /*
  * Transmit in auto_ack mode without request ack packet from RX device
  */
-uint8_t nrf24TransmitDataNoAck(uint8_t *data, uint8_t size);
+uint8_t nrf24_transmit_data_no_ack(uint8_t *data, uint8_t size);
+
 
 
 /*
  * Send ACk with payload packet as a receiver(RX) device
  */
-void nrf24TransmitRXPayloadAsRX(uint8_t pipe, uint8_t *data, uint8_t size);
+void nrf24_transmit_rx_payload_as_rx(uint8_t pipe, uint8_t *data, uint8_t size);
+
 
 
 /*
  * Detect signal on selected channel above -64dbm
  */
-uint8_t nrf24DetectSignalOnChannel(void);
+uint8_t nrf24_detect_signal_on_channel(void);
+
 
 
 /*
  * check if data is in RX FIFO
  */
-uint8_t nrf24DataAvailable(void);
+uint8_t nrf24_data_available(void);
+
 
 
 /*
  * Receive data
  */
-void nrf24ReceiveData(uint8_t *data, uint8_t size);
+void nrf24_receive_data(uint8_t *data, uint8_t size);
 
 
 //Will be soon
-void nrf24StartConstCarrier();
+void nrf24_start_const_carrier(void);
 
 
 //Will be soon
-void nrf24StopConstCarrier();
+void nrf24_stop_const_carrier(void);
 
 
 /*
  * Delay in micro seconds for more precision
  */
-void delayMicroseconds(uint16_t del_time);
+void nrf24_delay_microseconds(uint16_t del_time);
 
 
 #endif /* NRF24_H */
--- src/nrf24.c
+++ src/nrf24.c
@@ -16,7 +16,8 @@ extern SPI_HandleTypeDef hspiX;
 extern TIM_HandleTypeDef htimX;
 
 
-void writeCSNHIGH(void){
+void write_csn_high(void){
+
 	HAL_GPIO_WritePin(
 		CSN_GPIO_PORT,
 		CSN_GPIO_PIN,
@@ -24,7 +25,8 @@ void writeCSNHIGH(void){
 	);
 }
 
-void writeCSNLOW(void){
+void write_csn_low(void){
+
 	HAL_GPIO_WritePin(
 		CSN_GPIO_PORT,
 		CSN_GPIO_PIN,
@@ -32,7 +34,8 @@ void writeCSNLOW(void){
 	);
 }
 
-void writeCEHIGH(void){
+void write_ce_high(void){
+
 	HAL_GPIO_WritePin(
 		CE_GPIO_PORT,
 		CE_GPIO_PIN,
@@ -40,7 +43,8 @@ void writeCEHIGH(void){
 	);
 }
 
-void writeCELOW(void){
+void write_ce_low(void){
+
 	HAL_GPIO_WritePin(
 		CE_GPIO_PORT,
 		CE_GPIO_PIN,
@@ -48,80 +52,85 @@ void writeCELOW(void){
 	);
 }
 
-void nrf24WriteReg(uint8_t reg, uint8_t *data, uint8_t size){
+void nrf24_write_reg(uint8_t reg, uint8_t *data, uint8_t size){
+
 
 	uint8_t cmd = W_REGISTER | reg;
 
-	writeCSNLOW();
+	write_csn_low();
 
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Transmit(&hspiX, data, size, spi_w_timeout);
 
-	writeCSNHIGH();
+	write_csn_high();
 }
 
-uint8_t nrf24ReadReg(uint8_t reg, uint8_t size){
+uint8_t nrf24_read_reg(uint8_t reg, uint8_t size){
+
 	uint8_t cmd = R_REGISTER | reg;
 	uint8_t data = 0;
 
-	writeCSNLOW();
+	write_csn_low();
 
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Receive(&hspiX, &data, size, spi_r_timeout);
 
-	writeCSNHIGH();
+	write_csn_high();
 
 	return data;
 }
 
-void nrf24WriteSpecialCommand(uint8_t cmd){
+void nrf24_write_special_command(uint8_t cmd){
+
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 }
 
-void nrf24WriteSpecialReg(uint8_t *data, uint8_t size){
+void nrf24_write_special_reg(uint8_t *data, uint8_t size){
+
 	HAL_SPI_Transmit(&hspiX, data, size, spi_w_timeout);
 }
 
-void nrf24ReadSpecialReg(uint8_t *data, uint8_t size){
+void nrf24_read_special_reg(uint8_t *data, uint8_t size){
+
 	HAL_SPI_Receive(&hspiX, data, size, spi_r_timeout);
 }
 
-void nrf24PowerUp(void){
+void nrf24_power_up(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(CONFIG, 1);
+	data = nrf24_read_reg(CONFIG, 1);
 
 	data |= (1 << PWR_UP);
 
-	nrf24WriteReg(CONFIG, &data, 1);
+	nrf24_write_reg(CONFIG, &data, 1);
 }
 
-void nrf24PowerDown(void){
+void nrf24_power_down(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(CONFIG, 1);
+	data = nrf24_read_reg(CONFIG, 1);
 
 	data &= ~(1 << PWR_UP);
 
-	nrf24WriteReg(CONFIG, &data, 1);
+	nrf24_write_reg(CONFIG, &data, 1);
 }
 
-void nrf24SetTXPower(uint8_t pwr){
+void nrf24_set_tx_power(uint8_t pwr){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(RF_SETUP, 1);
+	data = nrf24_read_reg(RF_SETUP, 1);
 
 	data &= 184;
 
 	data |= (pwr << RF_PWR);
 
-	nrf24WriteReg(RF_SETUP, &data, 1);
+	nrf24_write_reg(RF_SETUP, &data, 1);
 }
 
-void nrf24SetDataRate(uint8_t bps){
+void nrf24_set_data_rate(uint8_t bps){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(RF_SETUP, 1);
+	data = nrf24_read_reg(RF_SETUP, 1);
 
 	data &= ~(1 << RF_DR_LOW) & ~(1 << RF_DR_HIGH);
 
@@ -131,181 +140,181 @@ void nrf24SetDataRate(uint8_t bps){
 		data |= (1 << RF_DR_LOW);
 	}
 
-	nrf24WriteReg(RF_SETUP, &data, 1);
+	nrf24_write_reg(RF_SETUP, &data, 1);
 }
 
-void nrf24SetChannel(uint8_t ch){
-	nrf24WriteReg(RF_CH, &ch, 1);
+void nrf24_set_channel(uint8_t ch){
+	nrf24_write_reg(RF_CH, &ch, 1);
 }
 
-void nrf24OpenTXPipe(uint8_t *addr){
-	nrf24WriteReg(TX_ADDR, addr, 5);
+void nrf24_open_tx_pipe(uint8_t *addr){
+	nrf24_write_reg(TX_ADDR, addr, 5);
 }
 
-void nrf24SetPipePayloadSize(uint8_t pipe, uint8_t size){
+void nrf24_set_pipe_payload_size(uint8_t pipe, uint8_t size){
 	if(size > 32){
 		size = 32;
 	}
 
 	switch(pipe){
 	case 0:
-		nrf24WriteReg(RX_PW_P0, &size, 1);
+		nrf24_write_reg(RX_PW_P0, &size, 1);
 
 		break;
 	case 1:
-		nrf24WriteReg(RX_PW_P1, &size, 1);
+		nrf24_write_reg(RX_PW_P1, &size, 1);
 
 		break;
 	case 2:
-		nrf24WriteReg(RX_PW_P2, &size, 1);
+		nrf24_write_reg(RX_PW_P2, &size, 1);
 
 		break;
 	case 3:
-		nrf24WriteReg(RX_PW_P3, &size, 1);
+		nrf24_write_reg(RX_PW_P3, &size, 1);
 
 		break;
 	case 4:
-		nrf24WriteReg(RX_PW_P4, &size, 1);
+		nrf24_write_reg(RX_PW_P4, &size, 1);
 
 		break;
 	case 5:
-		nrf24WriteReg(RX_PW_P5, &size, 1);
+		nrf24_write_reg(RX_PW_P5, &size, 1);
 
 		break;
 	}
 }
 
-void nrf24OpenRXPipe(uint8_t pipe, uint8_t *addr){
+void nrf24_open_rx_pipe(uint8_t pipe, uint8_t *addr){
 
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(EN_RXADDR, 1);
+	data = nrf24_read_reg(EN_RXADDR, 1);
 
 	switch(pipe){
 	case 0:
-		nrf24WriteReg(RX_ADDR_P0, addr, 5);
+		nrf24_write_reg(RX_ADDR_P0, addr, 5);
 
 		data |= (1 << ERX_P0);
 		break;
 	case 1:
-		nrf24WriteReg(RX_ADDR_P1, addr, 5);
+		nrf24_write_reg(RX_ADDR_P1, addr, 5);
 
 		data |= (1 << ERX_P1);
 		break;
 	case 2:
-		nrf24WriteReg(RX_ADDR_P2, addr, 1);
+		nrf24_write_reg(RX_ADDR_P2, addr, 1);
 
 		data |= (1 << ERX_P2);
 		break;
 	case 3:
-		nrf24WriteReg(RX_ADDR_P3, addr, 1);
+		nrf24_write_reg(RX_ADDR_P3, addr, 1);
 
 		data |= (1 << ERX_P3);
 		break;
 	case 4:
-		nrf24WriteReg(RX_ADDR_P4, addr, 1);
+		nrf24_write_reg(RX_ADDR_P4, addr, 1);
 
 		data |= (1 << ERX_P4);
 		break;
 	case 5:
-		nrf24WriteReg(RX_ADDR_P5, addr, 1);
+		nrf24_write_reg(RX_ADDR_P5, addr, 1);
 
 		data |= (1 << ERX_P5);
 		break;
 	}
 
-	nrf24WriteReg(EN_RXADDR, &data, 1);
+	nrf24_write_reg(EN_RXADDR, &data, 1);
 }
 
-void nrf24CloseRXPipe(uint8_t pipe){
-	uint8_t data = nrf24ReadReg(EN_RXADDR, 1);
+void nrf24_close_rx_pipe(uint8_t pipe){
+	uint8_t data = nrf24_read_reg(EN_RXADDR, 1);
 
 	data &= ~(1 << pipe);
 
-	nrf24WriteReg(EN_RXADDR, &data, 1);
+	nrf24_write_reg(EN_RXADDR, &data, 1);
 }
 
-void nrf24SetCRC(uint8_t en_crc, uint8_t crc0){
-	uint8_t data = nrf24ReadReg(CONFIG, 1);
+void nrf24_set_crc(uint8_t en_crc, uint8_t crc0){
+	uint8_t data = nrf24_read_reg(CONFIG, 1);
 
 	data &= ~(1 << EN_CRC) & ~(1 << CRCO);
 
 	data |= (en_crc << EN_CRC) | (crc0 << CRCO);
 
-	nrf24WriteReg(CONFIG, &data, 1);
+	nrf24_write_reg(CONFIG, &data, 1);
 }
 
-void nrf24SetAddressWidth(uint8_t bytes){
+void nrf24_set_address_width(uint8_t bytes){
 	bytes -= 2;
-	nrf24WriteReg(SETUP_AW, &bytes, 1);
+	nrf24_write_reg(SETUP_AW, &bytes, 1);
 }
 
-void nrf24FlushTX(void){
-	writeCSNLOW();
-	nrf24WriteSpecialCommand(FLUSH_TX);
-	writeCSNHIGH();
+void nrf24_flush_tx(void){
+	write_csn_low();
+	nrf24_write_special_command(FLUSH_TX);
+	write_csn_high();
 }
 
-void nrf24FlushRX(void){
-	writeCSNLOW();
-	nrf24WriteSpecialCommand(FLUSH_RX);
-	writeCSNHIGH();
+void nrf24_flush_rx(void){
+	write_csn_low();
+	nrf24_write_special_command(FLUSH_RX);
+	write_csn_high();
 }
 
-uint8_t nrf24ReadStatusReg(void){
+uint8_t nrf24_read_status_reg(void){
 	uint8_t data = 0;
 	uint8_t cmd = NOP;
 
-	writeCSNLOW();
+	write_csn_low();
 	HAL_SPI_TransmitReceive(&hspiX, &cmd, &data, 1, spi_rw_timeout);
-	writeCSNHIGH();
+	write_csn_high();
 
 	return data;
 }
 
-void nrf24ClearRX_DR(void){
+void nrf24_clear_rx_dr(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadStatusReg();
+	data = nrf24_read_status_reg();
 
 	data |= (1 << RX_DR);
 
-	nrf24WriteReg(STATUS, &data, 1);
+	nrf24_write_reg(STATUS, &data, 1);
 }
 
-void nrf24ClearTX_DS(void){
+void nrf24_clear_tx_ds(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadStatusReg();
+	data = nrf24_read_status_reg();
 
 	data |= (1 << TX_DS);
 
-    nrf24WriteReg(STATUS, &data, 1);
+    nrf24_write_reg(STATUS, &data, 1);
 }
 
-void nrf24ClearMAX_RT(void){
+void nrf24_clear_max_rt(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadStatusReg();
+	data = nrf24_read_status_reg();
 
 	data |= (1 << MAX_RT);
 
-    nrf24WriteReg(STATUS, &data, 1);
+    nrf24_write_reg(STATUS, &data, 1);
 }
 
-uint8_t nrf24ReadBit(uint8_t reg, uint8_t bit){
+uint8_t nrf24_read_bit(uint8_t reg, uint8_t bit){
 
-	if(nrf24ReadReg(reg, 1) & (1 << bit)){
+	if(nrf24_read_reg(reg, 1) & (1 << bit)){
 		return 1;
 	}
 
 	return 0;
 }
 
-void nrf24SetBit(uint8_t reg, uint8_t bit, uint8_t val){
+void nrf24_set_bit(uint8_t reg, uint8_t bit, uint8_t val){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(reg, 1);
+	data = nrf24_read_reg(reg, 1);
 
 	if(val){
 		data |= (1 << bit);
@@ -313,44 +322,44 @@ void nrf24SetBit(uint8_t reg, uint8_t bit, uint8_t val){
 		data &= ~(1 << bit);
 	}
 
-    nrf24WriteReg(reg, &data, 1);
+    nrf24_write_reg(reg, &data, 1);
 }
 
-uint8_t nrf24ReadPayloadWidth(void){
+uint8_t nrf24_read_payload_width(void){
 	uint8_t width = 0;
 
-	writeCSNLOW();
-	nrf24WriteSpecialCommand(R_RX_PL_WID);
-	nrf24ReadSpecialReg(&width, 1);
-	writeCSNHIGH();
+	write_csn_low();
+	nrf24_write_special_command(R_RX_PL_WID);
+	nrf24_read_special_reg(&width, 1);
+	write_csn_high();
 
 	return width;
 }
 
-void nrf24SwitchToRX(void){
+void nrf24_switch_to_rx(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(CONFIG, 1);
+	data = nrf24_read_reg(CONFIG, 1);
 
 	data |= (1 << PRIM_RX);
 
-	nrf24WriteReg(CONFIG, &data, 1);
+	nrf24_write_reg(CONFIG, &data, 1);
 
-	writeCEHIGH();
+	nrf24_write_ce_high();
 }
 
-void nrf24SwitchToTX(void){
+void nrf24_switch_to_tx(void){
 	uint8_t data = 0;
 
-	data = nrf24ReadReg(CONFIG, 1);
+	data = nrf24_read_reg(CONFIG, 1);
 
 	data &= ~(1 << PRIM_RX);
 
-	nrf24WriteReg(CONFIG, &data, 1);
+	nrf24_write_reg(CONFIG, &data, 1);
 }
 
-void nrf24EnableDynamicPayload(uint8_t en){
-	uint8_t feature = nrf24ReadReg(FEATURE, 1);
+void nrf24_enable_dynamic_payload(uint8_t en){
+	uint8_t feature = nrf24_read_reg(FEATURE, 1);
 
 	if(en == enable){
 		feature |= (1 << EN_DPL);
@@ -358,12 +367,12 @@ void nrf24EnableDynamicPayload(uint8_t en){
 		feature &= ~(1 << EN_DPL);
 	}
 
-	nrf24WriteReg(FEATURE, &feature, 1);
+	nrf24_write_reg(FEATURE, &feature, 1);
 }
 
-void nrf24SetRXDynamicPayloadPipe(uint8_t pipe, uint8_t en){
+void nrf24_set_rx_dynamic_payload_pipe(uint8_t pipe, uint8_t en){
 
-	uint8_t dynpd = nrf24ReadReg(DYNPD, 1);
+	uint8_t dynpd = nrf24_read_reg(DYNPD, 1);
 
 	if(pipe > 5){
 		pipe = 5;
@@ -375,16 +384,16 @@ void nrf24SetRXDynamicPayloadPipe(uint8_t pipe, uint8_t en){
 		dynpd &= ~(1 << pipe);
 	}
 
-	nrf24WriteReg(DYNPD, &dynpd, 1);
+	nrf24_write_reg(DYNPD, &dynpd, 1);
 }
 
-void nrf24EnableAutoAck(uint8_t pipe, uint8_t ack){
+void nrf24_enable_auto_ack(uint8_t pipe, uint8_t ack){
 
 	if(pipe > 5){
 		pipe = 5;
 	}
 
-	uint8_t enaa = nrf24ReadReg(EN_AA, 1);
+	uint8_t enaa = nrf24_read_reg(EN_AA, 1);
 
 	if(ack){
 		enaa |= (1 << pipe);
@@ -392,11 +401,11 @@ void nrf24EnableAutoAck(uint8_t pipe, uint8_t ack){
 		enaa &= ~(1 << pipe);
 	}
 
-	nrf24WriteReg(EN_AA, &enaa, 1);
+	nrf24_write_reg(EN_AA, &enaa, 1);
 }
 
-void nrf24EnableAutoAckOnAllPipes(uint8_t ack){
-	uint8_t enaa = nrf24ReadReg(EN_AA, 1);
+void nrf24_enable_auto_ack_on_all_pipes(uint8_t ack){
+	uint8_t enaa = nrf24_read_reg(EN_AA, 1);
 
 	if(ack){
 		enaa = 63;
@@ -404,11 +413,11 @@ void nrf24EnableAutoAckOnAllPipes(uint8_t ack){
 		enaa = 0;
 	}
 
-	nrf24WriteReg(EN_AA, &enaa, 1);
+	nrf24_write_reg(EN_AA, &enaa, 1);
 }
 
-void nrf24EnableAckPayload(uint8_t en){
-	uint8_t feature = nrf24ReadReg(FEATURE, 1);
+void nrf24_enable_ack_payload(uint8_t en){
+	uint8_t feature = nrf24_read_reg(FEATURE, 1);
 
 	if(en){
 		feature |= (1 << EN_ACK_PAY);
@@ -416,11 +425,11 @@ void nrf24EnableAckPayload(uint8_t en){
 		feature &= ~(1 << EN_ACK_PAY);
 	}
 
-	nrf24WriteReg(FEATURE, &feature, 1);
+	nrf24_write_reg(FEATURE, &feature, 1);
 }
 
-void nrf24EnableTransmitNoAck(uint8_t en){
-	uint8_t feature = nrf24ReadReg(FEATURE, 1);
+void nrf24_enable_transmit_no_ack(uint8_t en){
+	uint8_t feature = nrf24_read_reg(FEATURE, 1);
 
 	if(en){
 		feature |= (1 << EN_DYN_ACK);
@@ -428,36 +437,36 @@ void nrf24EnableTransmitNoAck(uint8_t en){
 		feature &= ~(1 << EN_DYN_ACK);
 	}
 
-	nrf24WriteReg(FEATURE, &feature, 1);
+	nrf24_write_reg(FEATURE, &feature, 1);
 }
 
-void nrf24SetAutoRetransmissionDelay(uint8_t delay){
-	uint8_t data = nrf24ReadReg(SETUP_RETR, 1);
+void nrf24_set_auto_retransmission_delay(uint8_t delay){
+	uint8_t data = nrf24_read_reg(SETUP_RETR, 1);
 
 	data &= 15;
 
 	data |= (delay << ARD);
 
-	nrf24WriteReg(SETUP_RETR, &data, 1);
+	nrf24_write_reg(SETUP_RETR, &data, 1);
 }
 
-void nrf24SetAutoRetransmissionLimit(uint8_t limit){
-	uint8_t data = nrf24ReadReg(SETUP_RETR, 1);
+void nrf24_set_auto_retransmission_limit(uint8_t limit){
+	uint8_t data = nrf24_read_reg(SETUP_RETR, 1);
 
 	data &= 240;
 
 	data |= (limit << ARC);
 
-	nrf24WriteReg(SETUP_RETR, &data, 1);
+	nrf24_write_reg(SETUP_RETR, &data, 1);
 }
 
-void nrf24ConvertTypeToUint8Array(size_t in, uint8_t* out, uint16_t size){
+void nrf24_convert_type_to_uint8_array(size_t in, uint8_t* out, uint16_t size){
 	for(uint16_t i = 0; i < size; i++){
 		out[i] = (((in & (255 << (i*8)))) >> (i*8));
 	}
 }
 
-size_t nrf24ConvertUint8ArrayToType(uint8_t* in, uint16_t size){
+size_t nrf24_convert_uint8_array_to_type(uint8_t* in, uint16_t size){
 	size_t out = 0;
 
 	for(uint16_t i = 0; i < size; i++){
@@ -468,55 +477,55 @@ size_t nrf24ConvertUint8ArrayToType(uint8_t* in, uint16_t size){
 }
 
 
-uint8_t nrf24TransmitData(uint8_t *data, uint8_t size){
+uint8_t nrf24_transmit_data(uint8_t *data, uint8_t size){
 
-	writeCELOW();
+	nrf24_write_ce_low();
 
 	uint8_t cmd = W_TX_PAYLOAD;
 
-	writeCSNLOW();
+	write_csn_low();
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Transmit(&hspiX, data, size, spi_w_timeout);
-	writeCSNHIGH();
+	write_csn_high();
 
-	writeCEHIGH();
-	delayMicroseconds(20);
-	writeCELOW();
+	nrf24_write_ce_high();
+	nrf24_delay_microseconds(20);
+	nrf24_write_ce_low();
 
-	if(nrf24ReadStatusReg() & (1 << MAX_RT)){
-		nrf24ClearMAX_RT();
-		nrf24FlushTX();
+	if(nrf24_read_status_reg() & (1 << MAX_RT)){
+		nrf24_clear_max_rt();
+		nrf24_flush_tx();
 		return 1;
 	}
 
 	return 0;
 }
 
-uint8_t nrf24TransmitDataNoAck(uint8_t *data, uint8_t size){
+uint8_t nrf24_transmit_data_no_ack(uint8_t *data, uint8_t size){
 
-	writeCELOW();
+	nrf24_write_ce_low();
 
 	uint8_t cmd = W_TX_PAYLOAD_NOACK;
 
-	writeCSNLOW();
+	write_csn_low();
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Transmit(&hspiX, data, size, spi_w_timeout);
-	writeCSNHIGH();
+	write_csn_high();
 
-	writeCEHIGH();
-	delayMicroseconds(20);
-	writeCELOW();
+	nrf24_write_ce_high();
+	nrf24_delay_microseconds(20);
+	nrf24_write_ce_low();
 
-	if(nrf24ReadStatusReg() & (1 << MAX_RT)){
-		nrf24ClearMAX_RT();
-		nrf24FlushTX();
+	if(nrf24_read_status_reg() & (1 << MAX_RT)){
+		nrf24_clear_max_rt();
+		nrf24_flush_tx();
 		return 1;
 	}
 
 	return 0;
 }
 
-void nrf24TransmitRXPayloadAsRX(uint8_t pipe, uint8_t *data, uint8_t size){
+void nrf24_transmit_rx_payload_as_rx(uint8_t pipe, uint8_t *data, uint8_t size){
 
 	if(pipe > 5){
 		pipe = 5;
@@ -524,20 +533,20 @@ void nrf24TransmitRXPayloadAsRX(uint8_t pipe, uint8_t *data, uint8_t size){
 
 	uint8_t cmd = (W_ACK_PAYLOAD | pipe);
 
-	writeCSNLOW();
+	write_csn_low();
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Transmit(&hspiX, data, size, spi_w_timeout);
-	writeCSNHIGH();
+	write_csn_high();
 
 }
 
-uint8_t nrf24DetectSignalOnChannel(void){
-	return nrf24ReadReg(RPD, 1);
+uint8_t nrf24_detect_signal_on_channel(void){
+	return nrf24_read_reg(RPD, 1);
 }
 
-uint8_t nrf24DataAvailable(void){
+uint8_t nrf24_data_available(void){
 
- 	uint8_t reg_dt = nrf24ReadReg(FIFO_STATUS, 1);
+ 	uint8_t reg_dt = nrf24_read_reg(FIFO_STATUS, 1);
 
 	if(!(reg_dt & (1 << RX_EMPTY))){
 		return 1;
@@ -546,20 +555,20 @@ uint8_t nrf24DataAvailable(void){
 	return 0;
 }
 
-void nrf24ReceiveData(uint8_t *data, uint8_t size){
+void nrf24_receive_data(uint8_t *data, uint8_t size){
 	uint8_t cmd = R_RX_PAYLOAD;
 
-	writeCSNLOW();
+	write_csn_low();
 	HAL_SPI_Transmit(&hspiX, &cmd, 1, spi_w_timeout);
 	HAL_SPI_Receive(&hspiX, data, size, spi_r_timeout);
-	writeCSNHIGH();
+	write_csn_high();
 
 	if(!nrf24ReadBit(FIFO_STATUS, RX_EMPTY)){
-		nrf24ClearRX_DR();
+		nrf24_clear_rx_dr();
 	}
 }
 
-void delayMicroseconds(uint16_t del_time){
+void nrf24_delay_microseconds(uint16_t del_time){
 	__HAL_TIM_SET_COUNTER(&htimX, 0);
 	uint16_t tmp_t = __HAL_TIM_GET_COUNTER(&htimX);
 	while((__HAL_TIM_GET_COUNTER(&htimX)-tmp_t) < del_time){
@@ -567,58 +576,61 @@ void delayMicroseconds(uint16_t del_time){
 	}
 }
 
-void nrf24StartConstCarrier(){
+void nrf24_start_const_carrier(void){
 	// TODO: implement whatever this was supposed to be
 }
 
-void nrf24StopConstCarrier(){
+
+void nrf24_stop_const_carrier(void){
 	// TODO: implement whatever this was supposed to be
 }
 
-void nrf24SetDefaults(void){
-	writeCELOW();
+void nrf24_set_defaults(void){
+	nrf24_write_ce_low();
 
-	nrf24PowerDown();
-	nrf24SetTXPower(3);
-	nrf24SetDataRate(_1mbps);
-	nrf24SetChannel(2);
-	nrf24SetCRC(no_crc, _1byte);
-	nrf24SetAddressWidth(5);
-	nrf24FlushTX();
-	nrf24FlushRX();
-	nrf24ClearRX_DR();
-	nrf24ClearTX_DS();
-	nrf24ClearMAX_RT();
-	nrf24SwitchToTX();
-	nrf24EnableDynamicPayload(disable);
-	nrf24EnableAckPayload(disable);
-	nrf24EnableTransmitNoAck(disable);
-	nrf24SetAutoRetransmissionDelay(0);
-	nrf24SetAutoRetransmissionLimit(3);
+
+	nrf24_power_down();
+
+	nrf24_set_tx_power(3);
+	nrf24_set_data_rate(_1mbps);
+	nrf24_set_channel(2);
+	nrf24_set_crc(no_crc, _1byte);
+	nrf24_set_address_width(5);
+	nrf24_flush_tx();
+	nrf24_flush_rx();
+	nrf24_clear_rx_dr();
+	nrf24_clear_tx_dr();
+	nrf24_clear_max_rt();
+	nrf24_switch_to_tx();
+	nrf24_enable_dynamic_payload(disable);
+	nrf24_enable_ack_payload(disable);
+	nrf24_enable_transmit_no_ack(disable);
+	nrf24_set_auto_retransmission_delay(0);
+	nrf24_set_auto_retransmission_limit(3);
 
 
 	for(uint8_t i = 0; i < 5; i++){
-		nrf24SetPipePayloadSize(i, 0);
-		nrf24CloseRXPipe(i);
-		nrf24SetRXDynamicPayloadPipe(i, disable);
-		nrf24EnableAutoAck(i, enable);
+		nrf24_set_pipe_payload_size(i, 0);
+		nrf24_close_rx_pipe(i);
+		nrf24_set_rx_dynamic_payload_pipe(i, disable);
+		nrf24_enable_auto_ack(i, enable);
 	}
 
-	writeCEHIGH();
+	nrf24_write_ce_high();
 }
 
-void nrf24Init(void){
+void nrf24_init(void){
 
 	if(HAL_TIM_Base_Start(&htimX) != HAL_OK){
 		// Error_Handler(); // TODO: add this when used in main.c
 	}
 
-	nrf24PowerUp();
+	nrf24_power_up();
 
-	nrf24FlushTX();
-	nrf24FlushRX();
+	nrf24_flush_tx();
+	nrf24_flush_rx();
 
-	nrf24ClearRX_DR();
-	nrf24ClearTX_DS();
-	nrf24ClearMAX_RT();
+	nrf24_clear_rx_dr();
+	nrf24_clear_tx_dr();
+	nrf24_clear_max_rt();
 }
